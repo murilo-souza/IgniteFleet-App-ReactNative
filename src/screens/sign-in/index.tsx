3032260a2e8551d493994afb53ad91dd7c438030
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-04-26 20:20:31 -0300

    feat: add button component

---
+++ src/screens/sign-in/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Slogan, Title } from './styles'
+import backgroundImg from '../../assets/background.png'
+import { Button } from '../../components/button'
+
+export function SignIn() {
+  return (
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
+      <Button title="Entrar com Google" />
+    </Container>
+  )
+}
