3fb14e1f792cc0a8da717ae21bac729da27e1761
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-04-30 19:15:47 -0300

    feat: ensure the components will stay in safe area

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import { StatusBar } from 'react-native'
 import { AppProvider, UserProvider } from '@realm/react'
 import { REALM_APP_ID } from '@env'
 import { Routes } from './src/routes'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -24,14 +25,16 @@ export default function App() {
   return (
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <UserProvider fallback={SignIn}>
-          <Routes />
-        </UserProvider>
+        <SafeAreaProvider>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <UserProvider fallback={SignIn}>
+            <Routes />
+          </UserProvider>
+        </SafeAreaProvider>
       </ThemeProvider>
     </AppProvider>
   )
--- src/components/home-header/index.tsx
+++ src/components/home-header/index.tsx
@@ -4,17 +4,21 @@ import { Power } from 'phosphor-react-native'
 import { useUser, useApp } from '@realm/react'
 
 import theme from '../../theme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export function HomeHeader() {
   const user = useUser()
   const app = useApp()
+
+  const insets = useSafeAreaInsets()
+  const paddingTop = insets.top + 32
 
   function handleLogout() {
     app.currentUser?.logOut()
   }
 
   return (
-    <Container>
+    <Container style={{ paddingTop }}>
       <Picture
         source={{ uri: user?.profile.pictureUrl }}
         placeholder="L184i9offQof00ayfQay~qj[fQj["
