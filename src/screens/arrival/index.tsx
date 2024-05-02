7ba95e207fd86f07dcfc789fbca03e480f8fb3c2
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-05-02 16:43:29 -0300

    feat: ensure user can delete a register

--- src/screens/arrival/index.tsx
+++ src/screens/arrival/index.tsx
@@ -7,12 +7,12 @@ import {
   Label,
   LicensePlate,
 } from './styles'
-import { useRoute } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { Header } from '../../components/header'
 import { Button } from '../../components/button'
 import { ButtonIcon } from '../../components/button-icon'
 import { X } from 'phosphor-react-native'
-import { useObject } from '../../libs/realm'
+import { useObject, useRealm } from '../../libs/realm'
 import { Historic } from '../../libs/realm/schemas/historic'
 import { BSON } from 'realm'
 import { Alert } from 'react-native'
@@ -25,7 +25,27 @@ export function Arrival() {
   const route = useRoute()
   const { id } = route.params as RouteParamsProps
 
+  const realm = useRealm()
+  const { goBack } = useNavigation()
   const historic = useObject(Historic, new BSON.UUID(id))
+
+  function handleRemoveVehicleUsage() {
+    Alert.alert(
+      'Cancelar',
+      'Deseja realmente cancelar a utilização do veículo?',
+      [
+        { text: 'Não', style: 'cancel' },
+        { text: 'Sim', onPress: () => removeVehicleUsage() },
+      ],
+    )
+  }
+
+  function removeVehicleUsage() {
+    realm.write(() => {
+      realm.delete(historic)
+    })
+    goBack()
+  }
 
   return (
     <Container>
@@ -39,7 +59,7 @@ export function Arrival() {
         <Description>{historic?.description}</Description>
 
         <Footer>
-          <ButtonIcon icon={X} />
+          <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
           <Button title="Registrar chegada" />
         </Footer>
       </Content>
